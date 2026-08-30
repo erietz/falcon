66d51752c7b1a633cfc6d1b53e6c60a0cabd5d0b
Ethan Rietz <ewrietz@gmail.com>
2026-08-30 14:53:59 -0500

    forgot to add file in last commit

---
+++ falcon.ts
@@ -0,0 +1,32 @@
+import { task, run } from "./src/index.ts";
+import { execFileSync } from "node:child_process";
+
+function sh(cmd: string, args: string[] = [], env: Record<string, string> = {}) {
+  execFileSync(cmd, args, {
+    env: { ...process.env, ...env },
+    stdio: "inherit",
+    encoding: "utf-8",
+  });
+}
+
+task("version", async () => {
+  const versionChangeTypes = ["major", "minor", "patch"];
+  const versionChangeType = process.env.V || "";
+  if (!versionChangeTypes.includes(versionChangeType)) {
+    throw new Error(
+      `Invalid version change type: ${versionChangeType}. Must be one of: ${versionChangeTypes.join(
+        ", "
+      )}`
+    );
+  }
+
+  sh("npm", ["version", versionChangeType, "-m", `Release v%s`]);
+  sh("git", ["push", "origin", "main", "--follow-tags"]);
+
+});
+
+task("publish", ["version"], async () => {
+  sh("npm", ["publish", "--access", "public"]);
+});
+
+run();
