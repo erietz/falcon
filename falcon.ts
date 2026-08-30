d90c81d6a53bcf52880e00b1987facd31ff8cfb2
Ethan Rietz <ewrietz@gmail.com>
2026-08-30 15:03:48 -0500

    remove ugly stack trace

--- falcon.ts
+++ falcon.ts
@@ -2,22 +2,27 @@ import { task, run } from "./src/index.ts";
 import { execFileSync } from "node:child_process";
 
 function sh(cmd: string, args: string[] = [], env: Record<string, string> = {}) {
-  execFileSync(cmd, args, {
-    env: { ...process.env, ...env },
-    stdio: "inherit",
-    encoding: "utf-8",
-  });
+  try {
+    execFileSync(cmd, args, {
+      env: { ...process.env, ...env },
+      stdio: "inherit",
+      encoding: "utf-8",
+    });
+  } catch (error) {
+    process.exit(1);
+  }
 }
 
 task("version", () => {
   const versionChangeTypes = ["major", "minor", "patch"];
   const versionChangeType = process.env.V || "";
   if (!versionChangeTypes.includes(versionChangeType)) {
-    throw new Error(
+    console.error(
       `Invalid version change type: V=${versionChangeType}. Must be one of: ${versionChangeTypes.join(
         ", "
       )}`
     );
+    process.exit(1);
   }
 
   sh("npm", ["version", versionChangeType, "-m", `Release v%s`]);
